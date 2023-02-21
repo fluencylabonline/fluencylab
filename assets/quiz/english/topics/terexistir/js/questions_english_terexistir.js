e5c5814fc78ca58c447e82f1196c08eed0324e1a
fluencylabonline <124739764+fluencylabonline@users.noreply.github.com>
2023-02-21 15:53:54 -0300

    Add files via upload

---
+++ assets/quiz/english/topics/terexistir/js/questions_english_terexistir.js
@@ -0,0 +1,188 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+
+  {
+    numb: 1,
+    question: "Which verb is used to show possession?",
+    answer: "have",
+    options: [
+    "there",
+    "be",
+    "have",
+    "none of the above"
+    ]
+    },
+    {
+    numb: 2,
+    question: "Which verb is used to describe the existence of something?",
+    answer: "there to be",
+    options: [
+    "have",
+    "do",
+    "be",
+    "there to be"
+    ]
+    },
+    {
+    numb: 3,
+    question: "I _____ a book in my bag.",
+    answer: "have",
+    options: [
+    "there is",
+    "there are",
+    "have",
+    "be"
+    ]
+    },
+    {
+    numb: 4,
+    question: "_____ a pen on the desk?",
+    answer: "Is there",
+    options: [
+    "Are there",
+    "Is there",
+    "There is",
+    "There are"
+    ]
+    },
+    {
+    numb: 5,
+    question: "_____ any milk in the fridge?",
+    answer: "Is there",
+    options: [
+    "Are there",
+    "Is there",
+    "There is",
+    "There are"
+    ]
+    },
+    {
+    numb: 6,
+    question: "I _____ a headache.",
+    answer: "have",
+    options: [
+    "there is",
+    "there are",
+    "have",
+    "be"
+    ]
+    },
+    {
+    numb: 7,
+    question: "There _____ a party tonight.",
+    answer: "is",
+    options: [
+    "are",
+    "isn't",
+    "aren't",
+    "none of the above"
+    ]
+    },
+    {
+    numb: 8,
+    question: "_____ any water in the bottle?",
+    answer: "Is there",
+    options: [
+    "Are there",
+    "Is there",
+    "There is",
+    "There are"
+    ]
+    },
+    {
+    numb: 9,
+    question: "I _____ two brothers and one sister.",
+    answer: "have",
+    options: [
+    "there is",
+    "there are",
+    "have",
+    "be"
+    ]
+    },
+    {
+    numb: 10,
+    question: "_____ a library near here?",
+    answer: "Is there",
+    options: [
+    "Are there",
+    "Is there",
+    "There is",
+    "There are"
+    ]
+    },
+    {
+    numb: 11,
+    question: "There _____ some bread in the basket.",
+    answer: "is",
+    options: [
+    "are",
+    "isn't",
+    "aren't",
+    "none of the above"
+    ]
+    },
+    {
+    numb: 12,
+    question: "_____ a hotel in this city?",
+    answer: "Is there",
+    options: [
+    "Are there",
+    "Is there",
+    "There is",
+    "There are"
+    ]
+    },
+    {
+    numb: 13,
+    question: "I _____ a meeting at 10 am tomorrow.",
+    answer: "have",
+    options: [
+    "there is",
+    "there are",
+    "have",
+    "be"
+    ]
+    },
+    {
+    numb: 14,
+    question: "_____ a movie theater in this area?",
+    answer: "Is there",
+    options: [
+    "Are there",
+    "Is there",
+    "There is",
+    "There are"
+    ]
+    },
+
+    {
+      numb: 15,
+      question: "_____ any fruit juice in the fridge?",
+      answer: "Is there",
+      options: [
+      "Are there",
+      "Is there",
+      "There is",
+      "There are"
+      ]
+      }
+];
+
+/*
+I need 3 English questions with 4 options each. The topic is: simple grammar questions for begginers. 
+The following structure must be followed:
+
+{
+    numb: "number",
+    question: "question goes here",
+    answer: "correct answer goes here, exactly how it is in the option",
+    options: [
+    "option 1",
+    "option 2",
+    "option 3",
+    "option 4"
+    ]
+    },
+
+    */
