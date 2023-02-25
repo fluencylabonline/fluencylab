0b03dc131f5746a84ab9fa06f1c3d930751de480
fluencylabonline <124739764+fluencylabonline@users.noreply.github.com>
2023-02-24 21:49:00 -0300

    Add files via upload

---
+++ login/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwaOF7DmqWykMvgAqZdKdzToY6jWpLGiQ",
+    authDomain: "fluencylab-online.firebaseapp.com",
+    projectId: "fluencylab-online",
+    storageBucket: "fluencylab-online.appspot.com",
+    messagingSenderId: "1048611238645",
+    appId: "1:1048611238645:web:94ed80782ec076c7230bbc"
+  };
+    firebase.initializeApp(firebaseConfig);
